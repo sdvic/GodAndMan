c64ae292bb053b020e8b1a0665d901bee8cb416e
vicwintriss <sdvic@me.com>
2018-09-11 20:09:06 -0700

    Version 0.9  Working circles and lines

--- src/com/wintrisstech/GodAndMan.java
+++ src/com/wintrisstech/GodAndMan.java
@@ -1,6 +1,6 @@
 package com.wintrisstech;
 /**************************************************************************
- * Initial ver 0.3 9/3/18
+ * Initial ver 0.9 9/11/18
  * Copyright 2018 Vic Wintriss
  **************************************************************************/
 
@@ -8,10 +8,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class GodAndMan extends JComponent implements ActionListener, Runnable
@@ -24,14 +25,11 @@ public class GodAndMan extends JComponent implements ActionListener, Runnable
     private ArrayList<Ellipse2D.Double> circles = new ArrayList<Ellipse2D.Double>();
     private ArrayList<Line2D.Double> lines = new ArrayList<Line2D.Double>();
     private Timer paintTicker = new Timer(500, this); //Ticks every 20 milliseconds (50 times per second); calls on actionPerformed() when it ticks.
-    private AffineTransform universeOffset = new AffineTransform();
-    private int q = 0;
-    private int outerRingRadius = heightOfScreen/2;
+    private int outerRingRadius = heightOfScreen / 2;
     private int outerRingDiameter = heightOfScreen;
-    private int xOffset = heightOfScreen/2;
-    private int yOffset = heightOfScreen/2;
     private int innerRingRadius;
     private int innerRingDiameter;
+    private int theta;
 
     public static void main(String[] args)
     {
@@ -47,22 +45,20 @@ public class GodAndMan extends JComponent implements ActionListener, Runnable
         mainGameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainGameWindow.setVisible(true);
         paintTicker.start();
-        for ( innerRingDiameter = 0; innerRingDiameter < outerRingDiameter; innerRingDiameter += 10)
+        for (innerRingDiameter = 0; innerRingDiameter < outerRingDiameter + 10; innerRingDiameter += 10)
         {
-            innerRingRadius = innerRingDiameter/2;
+            innerRingRadius = innerRingDiameter / 2;
             circles.add(new Ellipse2D.Double(outerRingRadius - innerRingRadius, outerRingRadius - innerRingRadius, innerRingDiameter, innerRingDiameter));
         }
-        for (int i = 0; i < 360; i++)
+        int i = 0;
+        for (theta = 0; theta < 360; theta += 1) // Degrees
         {
-            int x1 = (int) ((Math.cos(Math.toRadians(i)) * outerRingRadius)) + xOffset;
-            int y1 = (int) -((Math.sin(Math.toRadians(i)) * outerRingRadius)) + yOffset;
-            lines.add(new Line2D.Double(x1 + outerRingRadius, y1 + outerRingRadius, outerRingRadius, outerRingRadius));
+            int y = (int) (outerRingRadius - (outerRingRadius * Math.sin(Math.toRadians(theta))));
+            int x = (int) (outerRingRadius + (outerRingRadius * Math.cos(Math.toRadians(theta))));
+            Line2D.Double newLine = (new Line2D.Double(x, y, outerRingRadius, outerRingRadius));
+            lines.add(newLine);
         }
-        for (Line2D.Double line : lines)
-        {
-            System.out.println("line[" + lines.indexOf(line) + "]  x1/y1 = " + line.getX1() + "/" + line.getY1() + " x2/y2 = " + line.getX2() + "/" + line.getY2());
-        }
-        System.out.println("GodAndMan.run terminated");
+        System.out.println("GodAndMan.run finished normally");
     }
 
     public void paint(Graphics g)
@@ -75,19 +71,15 @@ public class GodAndMan extends JComponent implements ActionListener, Runnable
         {
             g2.draw(ellipse);
         }
-        q+=10;
         for (Line2D.Double line : lines)
         {
-            g2.draw(lines.get(q));
-            g2.setFont(new Font("Bank Gothic", Font.BOLD, 99));
-            g2.drawString("Angle = " + q, 1500, 100 );
+            g2.draw(line);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        System.out.println("tick, " + q + " degrees");
         repaint();
     }
 }
